c60366de6d6f56292994ce2ab8c5e9e4e1be8cba
Héctor C. Alzate <halzate93@gmail.com>
2016-11-03 01:02:31 -0500

    Initial layout

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.doodle-container').height($(window).height () - $('.doodle-container').offset ().top);
+    $('.doodle-container').width($('.doodle-container').parent().width ());
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
