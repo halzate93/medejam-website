8eaa50883a0a2d506e1ac05ed7b3a6e918f07b22
haderman <hadermania@gmail.com>
2016-11-05 15:46:18 -0500

    scroll target is not covered by the navbar

--- js/init.js
+++ js/init.js
@@ -1,3 +1,5 @@
+
+
 (function($){
   $(function(){
 
@@ -7,5 +9,23 @@
     $('.doodle-container').width($('.doodle-container').parent().width());
   }); // end of document ready
 
+  /* scroll's efect */
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+        var NAV_SIZE = 50
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - NAV_SIZE
+          }, 500, 'swing');
+          return false;
+        }
+      }
+    });
+  });
 
 })(jQuery); // end of jQuery name space
